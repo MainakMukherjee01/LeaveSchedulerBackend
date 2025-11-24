79c7742cb7a32c5ca3106ee0e722752502605d1f
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-11-24 11:17:55 +0530

    Fix Log Config

--- src/main/java/com/sap/fsad/leaveApp/config/SwaggerConfig.java
+++ src/main/java/com/sap/fsad/leaveApp/config/SwaggerConfig.java
@@ -1,12 +1,15 @@
 package com.sap.fsad.leaveApp.config;
 
-import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Leave Scheduler API", version = "1.0", description = "Enterprise-grade REST API for managing leave applications, policies, users, and notifications."))
@@ -20,6 +23,12 @@ public class SwaggerConfig {
                         .version("1.0")
                         .description("This API allows users within an organization to manage leave workflows securely.")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token obtained from /api/auth/login")));
     }
 }
